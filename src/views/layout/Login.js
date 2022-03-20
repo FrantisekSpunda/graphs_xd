9b29a563ac01412cfb410e1fc3cbf16c5b7cf1f8
frantisek.spunda <spundafr@gmail.com>
2022-03-20 23:02:29 +0100

    some casule styles

--- src/views/layout/Login.js
+++ src/views/layout/Login.js
@@ -1,15 +1,16 @@
 import logo from '../../graphs_logo.png'
 
+
 function Login () {
 
     return (
-        <div className='flex justify-center'>
-            <img src={logo} alt="logo" />
-            <h2>Anyone like statistick, but everyone need it.</h2>
+        <div className='flex flex-col items-center justify-center h-screen'>
+            <img src={logo} alt="logo" className='w-3/4 mb-5' />
+            <h2 className='mb-32'>Anyone like statistick, but everyone need it.</h2>
 
-            <form action="" className="login_form">
-                <input type="text" name="username" placeholder="Username" />    
-                <input type="password" name="password" placeholder="Password" />
+            <form action="" className="flex flex-col w-1/5">
+                <input className='p-2 rounded border' type="text" name="username" placeholder="Username" />    
+                <input className='p-2 rounded border' type="password" name="password" placeholder="Password" />
             </form>
         </div>
     )
