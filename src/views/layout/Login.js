5f177fa8d1c8059e44bfce504f945037c6c86e7a
frantisek.spunda <spundafr@gmail.com>
2022-03-20 22:38:00 +0100

    packege.json repair error

--- src/views/layout/Login.js
+++ src/views/layout/Login.js
@@ -3,14 +3,14 @@ import logo from '../../graphs_logo.png'
 function Login () {
 
     return (
-        <div className='align-center'>
-        <img src={logo} alt="logo" />
-        <h2>Anyone like statistick, but everyone need it.</h2>
+        <div className='flex align-center'>
+            <img src={logo} alt="logo" />
+            <h2>Anyone like statistick, but everyone need it.</h2>
 
-        <form action="" className="login_form">
-            <input type="text" name="username" placeholder="Username" />    
-            <input type="password" name="password" placeholder="Password" />
-        </form>
+            <form action="" className="login_form">
+                <input type="text" name="username" placeholder="Username" />    
+                <input type="password" name="password" placeholder="Password" />
+            </form>
         </div>
     )
 }
