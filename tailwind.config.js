d3ed6383c228eacc0a27245dc7fc28efbe8171d5
frantisek.spunda <frantisek.spunda@olc.cz>
2022-03-23 11:09:06 +0100

    add sass

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,20 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     fontFamily: {
+      colors: {
+        white: '#FFFFFF',
+        grey: {
+          100: '#F0F0F0',
+          300: '#C4C4C4',
+          900: '#575757'
+        },
+        orage: {
+          300: '#FFBF75',
+          500: '#FFA24B',
+          900: '#D7562E'
+        },
+        black: '#000000'
+      },
       sans: ['Poppins', ...defaultTheme.fontFamily.sans]
     }
   },
