540520b7d4549e3237b1a602dff6b31577d87c17
frantisek.spunda <spundafr@gmail.com>
2022-03-26 11:24:40 +0100

    navbar edit

--- src/views/core/Navbar.js
+++ src/views/core/Navbar.js
@@ -1,16 +1,24 @@
-import logo from '../../assets/images/logo-256.png';
 import { NavLink } from 'react-router-dom';
+
+import logo from '../../assets/images/logo-256.png';
+import profileImage from '../../assets/images/profile/alex-profile-image.png'
+import arrowDown from '../../assets/images/arrow2-down.png'
 
 function Navbar() {
     const navLinkClass = 'font-semibold text-sm '
 
     return (
-        <nav className="w-full h-16 px-6 py-4 border-grey-100 border-b flex justify-between">
+        <nav className="w-full h-18 px-6 py-4 border-grey-100 border-b flex justify-between items-center">
             <img src={logo} alt="logo" className='h-full' />
             <ul className='flex justify-between w-1/3 items-center'>
                 <NavLink to='register' className={({isActive}) => isActive ? navLinkClass + 'border-b border-gray-300' : navLinkClass}>Register</NavLink>
                 <NavLink to='login' className={({isActive}) => isActive ? 'border-b border-gray-300' : navLinkClass}>Login</NavLink>
-                <NavLink to='account' className={({isActive}) => isActive ? navLinkClass + 'border-b border-gray-300' : navLinkClass}>matej.opicka</NavLink>
+                <div className='h-full flex justify-between items-center w-48'>
+                    <img src={arrowDown} className='cursor-pointer w-5' alt="arrow right" />
+                    <NavLink to='account' className={({isActive}) => isActive ? navLinkClass + 'border-b border-gray-300' : navLinkClass}>matej.opicka</NavLink>
+                    <img className='h-12 w-12 rounded-full' src={profileImage} alt="profile img" />
+
+                </div>
             </ul>
         </nav>
     )
